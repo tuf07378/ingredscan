14850bfad0df3c9cc52ee4cb356953931429a5db
VladamirC <tuf02502@temple.edu>
2016-04-21 14:14:58 -0400

    Fixed the results screen to work with no OCR text.

--- Ingredient Scanner/src/main/java/edu/sfsu/cs/orange/ocr/ResultsActivity.java
+++ Ingredient Scanner/src/main/java/edu/sfsu/cs/orange/ocr/ResultsActivity.java
@@ -17,10 +17,8 @@ import edu.sfsu.cs.orange.ocr.database.DataBaseHelper;
 
 public class ResultsActivity extends AppCompatActivity {
 
-    private TextView text2;
     private TextView resultsView;
-    private TextView ingredientsNotFoundView;
-    private TextView ocrTextView;
+    private String ocrText = "";
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,49 +32,48 @@ public class ResultsActivity extends AppCompatActivity {
 
         //Code to get the OCR text from the previous activity.
         Intent intent = getIntent();
-        String ocrText, food;
-        ocrText = food = "";
+
         if (intent.getExtras() != null) {
             ocrText = intent.getExtras().getString("ocrText");
         }
 
-        //Initializes our 3 textViews.
+        //Initializes our TextView
         resultsView = (TextView) findViewById(R.id.results_view);
-        ingredientsNotFoundView = (TextView) findViewById(R.id.ingredients_not_found);
-        ocrTextView = (TextView) findViewById(R.id.ocr_text);
 
+        resultCompute();  //
 
-        //Sets up our first textView, the OCR text.
-        ocrTextView.setText("OCR Scan Result:\n" + ocrText);
-
-
-        //Database procedure
-        DataBaseHelper myDbHelper = new DataBaseHelper(this);
-        myDbHelper = new DataBaseHelper(this);
-        try {
-            myDbHelper.createDataBase();
-        } catch (IOException ioe) {
-            throw new Error("Unable to create database");
-        }
-        try {
-            myDbHelper.openDataBase();
-        } catch (SQLException sqle) {
-            throw sqle;
-        }
-
-        //Allergens (hardcoded)
+        //Allergens (hardcoded, useful for testing)
         /*String[] allergensHardcoded = {"Artificial Sweetener", "Carcinogenic", "Dairy", "Egg", "Fish",
                 "Meat", "Peanut", "Sesame", "Shellfish", "Soy", "Sweetener", "Trans Fat", "Tree Nut", "Wheat"};
                 */
-        String[] allergens = allergenArray();
-        String mainResultsInfo = myDbHelper.resultsInfo(ocrText, allergens);
-        String ingredientsNotFound = myDbHelper.foodNotFoundList(ocrText);
+    }
 
-        //Puts the main ingredient allergen results in the resultsView, and lists the ingredients not found
-        resultsView.setText(mainResultsInfo);
-        ingredientsNotFoundView.setText(ingredientsNotFound);
-        myDbHelper.close(); //where I fixed the sql error :)<3333
 
+    //Main Method that gives us our results and puts them in the TextView.
+    public void resultCompute()
+    {
+        if(ocrText.length() > 0) {
+            DataBaseHelper myDbHelper = new DataBaseHelper(this);
+            myDbHelper = new DataBaseHelper(this);
+            try {
+                myDbHelper.createDataBase();
+            } catch (IOException ioe) {
+                throw new Error("Unable to create database");
+            }
+            try {
+                myDbHelper.openDataBase();
+            } catch (SQLException sqle) {
+                throw sqle;
+            }
+            String[] allergens = allergenArray();
+            String mainResultsInfo = myDbHelper.resultsInfo(ocrText, allergens);
+            String ingredientsNotFound = myDbHelper.foodNotFoundList(ocrText);
+            resultsView.setText("OCR Text: " + ocrText + "\nResult: " + mainResultsInfo + "\n" + ingredientsNotFound);
+            myDbHelper.close(); //where I fixed the sql error :)<3333
+        }
+        else{
+            resultsView.setText("No text scanned. Go to the camera and scan some text.");
+        }
     }
 
     public String[] allergenArray() {
