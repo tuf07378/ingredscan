0487974723abc8937f89af52913e0b31ec4180bf
VladamirC <tuf02502@temple.edu>
2016-04-23 18:47:24 -0400

    method now replaces all "." with "," in our app, since ingredients don't use periods, and it's a common misreading for a ",".

--- Ingredient Scanner/src/main/java/edu/sfsu/cs/orange/ocr/database/DataBaseHelper.java
+++ Ingredient Scanner/src/main/java/edu/sfsu/cs/orange/ocr/database/DataBaseHelper.java
@@ -203,6 +203,7 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     public String[] stringtoArray(String s)
     {
         String replacedStr = s.replaceAll("\n", " ");
+        replacedStr = replacedStr.replaceAll(".",","); //replaces all periods with commas.
         replacedStr = replacedStr.replaceAll("     ", " ");  //sets 5 spaces to 1 space :>
         replacedStr = replacedStr.replaceAll("    ", " ");  //sets 4 spaces to 1 space
         replacedStr = replacedStr.replaceAll("   ", " ");  //sets 3 spaces to 1 space
